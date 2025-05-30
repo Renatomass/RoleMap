de1d8ac13e008517ed46b3fbd449559662dab77c
RenatoLima <contatorenatolima@outlook.com>
2025-05-30 17:31:10 -0300

    muita coisa feita kk

--- src/components/CardLocal.jsx
+++ src/components/CardLocal.jsx
@@ -8,7 +8,8 @@ export default function CardLocal({ imagem, nome, nota, distancia }) {
   const [mostrarModalAceito, setMostrarModalAceito] = useState(false);
   const [mostrarModalRecusado, setMostrarModalRecusado] = useState(false);
   const [mostrarModalNaoVotou, setMostrarModalNaoVotou] = useState(false);
-  const [votou, setVotou] = useState(false); 
+  const [votou, setVotou] = useState(false);
+  const [votoEmProgresso, setVotoEmProgresso] = useState("");
 
   const handleAlerta = () => {
     if (!votou) {
@@ -18,16 +19,19 @@ export default function CardLocal({ imagem, nome, nota, distancia }) {
 
   const votarAceitar = () => {
     setVotou(true);
+    setVotoEmProgresso("aceito");
     setMostrarModalAceito(true);
   };
 
   const votarRecusar = () => {
     setVotou(true);
+    setVotoEmProgresso("recusado");
     setMostrarModalRecusado(true);
   };
 
   const votarNoPopup = (aceitou) => {
     setVotou(true);
+    setVotoEmProgresso(aceitou ? "aceito" : "recusado");
     setMostrarModalNaoVotou(false);
     if (aceitou) {
       setMostrarModalAceito(true);
@@ -37,7 +41,7 @@ export default function CardLocal({ imagem, nome, nota, distancia }) {
   };
 
   return (
-    <div className="w-full h-100 mt-10 sm:mt-10 max-w-sm rounded-3xl shadow-2xl text-white relative overflow-hidden">
+    <div className="w-full h-100 mt-10 sm:mt-2 max-w-sm rounded-3xl shadow-2xl text-white relative overflow-hidden">
       <div className="absolute inset-0 bg-gradient-to-b from-[#000000] to-[#0004ff] z-0"></div>
       <img
         src={imagem}
@@ -48,30 +52,47 @@ export default function CardLocal({ imagem, nome, nota, distancia }) {
       <div className="relative z-10 px-4 pt-4 pb-6">
         <Timer tempoInicial={30} onAlerta={handleAlerta} />
         <div className="relative mt-50">
-          <h2 className="text-2xl px-2 font-bold drop-shadow-md">{nome}</h2>
-          <div className="flex justify-between px-3 text-base font-pdr font-bold text-purple-200 my-1.5">
+          <h2 className="flex justify-center text-2xl px-2 font-bold drop-shadow-md">{nome}</h2>
+          <div className="flex justify-center px-3 text-base font-pdr font-bold text-purple-200 my-1.5">
             <span className="flex items-center gap-1">⭐ {nota}</span>
             <span className="flex items-center gap-1">📍{distancia}</span>
           </div>
         </div>
-        <div className="flex gap-4 mt-3">
-          <button
-            onClick={votarAceitar}
-            className="flex-1 bg-green-500 hover:bg-green-400 text-white font-semibold py-2 rounded-xl transition-all shadow-md cursor-pointer"
-          >
-            ✅ Aceitar
-          </button>
-          <button
-            onClick={votarRecusar}
-            className="flex-1 bg-red-600 hover:bg-red-800 text-white font-semibold py-2 rounded-xl transition-all shadow-md cursor-pointer"
-          >
-            ❌ Recusar
-          </button>
-        </div>
+        {!votou ? (
+          <div className="flex gap-4 mt-3">
+            <button
+              onClick={votarAceitar}
+              className="flex-1 bg-green-500 hover:bg-green-400 text-white font-semibold py-2 rounded-xl transition-all shadow-md cursor-pointer"
+            >
+              ✅ Aceitar
+            </button>
+            <button
+              onClick={votarRecusar}
+              className="flex-1 bg-red-600 hover:bg-red-800 text-white font-semibold py-2 rounded-xl transition-all shadow-md cursor-pointer"
+            >
+              ❌ Recusar
+            </button>
+          </div>
+        ) : (
+          <div className="flex abosolute justify-center items-start mt-4">
+            <div className="flex items-center gap-3 bg-[#ffffff22] px-4 py-2 rounded-xl shadow-inner">
+              <div className="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin" />
+              <span className="text-sm font-semibold text-white">
+                {votoEmProgresso === "aceito"
+                  ? "Você aceitou!"
+                  : "Você recusou!"}
+              </span>
+            </div>
+          </div>
+        )}
       </div>
 
-      {mostrarModalAceito && <ModalAceito onFechar={() => setMostrarModalAceito(false)} />}
-      {mostrarModalRecusado && <ModalRecusado onFechar={() => setMostrarModalRecusado(false)} />}
+      {mostrarModalAceito && (
+        <ModalAceito onFechar={() => setMostrarModalAceito(false)} />
+      )}
+      {mostrarModalRecusado && (
+        <ModalRecusado onFechar={() => setMostrarModalRecusado(false)} />
+      )}
       {mostrarModalNaoVotou && (
         <ModalNaoVotou
           onAceitar={() => votarNoPopup(true)}
